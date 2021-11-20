2bb9cc7fe97bdfc67d9d81e3fad92898bafd94f7
MauroKinderknecht <56045994+MauroKinderknecht@users.noreply.github.com>
2021-11-20 11:28:24 -0300

    Initial commit

---
+++ src/main/java/Starships.java
@@ -0,0 +1,11 @@
+import edu.austral.dissis.starships.game.GameContext;
+
+public class Starships {
+
+    GameContext context;
+
+    public Starships(GameContext context) {
+        this.context = context;
+    }
+    
+}
